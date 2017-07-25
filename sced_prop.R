26a7a81bd2d2848e5d1aeca1d7c9318ff6739119
Maria Jacob <mj1416@ic.ac.uk>
2017-07-25 12:11:53 +0100

    thesis stuff

--- sced_prop.R
+++ sced_prop.R
@@ -12,7 +12,7 @@ n = dim(simple)[1]
 m = dim(simple)[2]
 
 N = n*m
-k = 369#floor(N*0.08)
+k = 24000#floor(N*0.08)
 
 threshold = qwe[N-k+1]
 
@@ -21,8 +21,8 @@ c_est <- rowSums(simple>threshold)/k
 require(graphics)
 filename <- "hh_sced_prop_full.pdf"
 pdf(file=filename,width = 12,paper = "a4r")
-plot(c_est,type ="p",pch="*",ylab=expression(hat(c)),xlab="Day",main="7 weeks prop")
-title(ylab=expression(hat(c)),mgp=c(2.65,1,0))
-#axis(side=1,at=seq(1,n,by=48*10),labels=seq(593,593+49,by=10))
+plot(c_est,type ="p",pch="*",ylab=expression(hat(p)),xaxt="n" ,mgp=c(2.65,1,0),xlab="Day")
+#title(ylab=expression(hat(p)),mgp=c(2.65,1,0))
+axis(side=1,at=c(1,481,961,1441,1921,n-24),labels=c(593,603,613,623,633,641))
 #axis(side=2,at=c(0,0.0004,0.0008,0.0012),labels = c(0,0.4,0.8,1.2))
 dev.off()
